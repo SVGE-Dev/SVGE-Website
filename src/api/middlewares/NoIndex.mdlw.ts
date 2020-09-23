c44d55ae3210248ecbd61375fd5666cfe8c36f3c
Dief Bell <diefenbaker.bell@gmail.com>
2020-09-23 09:08:38 +0100

    Middleware for preventing SEO indexing of pages

---
+++ src/api/middlewares/NoIndex.mdlw.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { ExpressMiddlewareInterface } from "routing-controllers";
+
+type cb = (err? : any) => any;
+
+export class SeoNoIndex implements ExpressMiddlewareInterface
+{
+	use(req : Request, res : Response, next? : cb) : any
+	{
+        res.setHeader("X-Robots-Tag", "noindex");
+        next();
+    }
+
+}
